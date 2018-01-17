e8ddc65ef665347e259bb2e3ce34a7223fafbfd5
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-01-17 09:16:46 -0500

    Method signature and swapper

---
+++ ArrayMethods/src/ArrayMethods1.java
@@ -0,0 +1,34 @@
+
+public class ArrayMethods1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public static void insertionSort(int[] list1)
+	{
+		
+	}
+	
+	
+	public static void selectionSort(double[] list1)
+	{
+		
+	}
+	
+	public static void bubbleSort(String[] list1)
+	{
+		
+	}
+	
+	public static void swapInt(int index1, int index2)
+	{
+		int temp = index1;
+		index1 = index2;
+		index2 = temp;
+	}
+
+}

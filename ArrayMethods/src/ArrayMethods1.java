8cea13487cdba566d86fb4c8735b56d2daf1d948
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-01-19 09:22:52 -0500

    :0

--- ArrayMethods/src/ArrayMethods1.java
+++ ArrayMethods/src/ArrayMethods1.java
@@ -5,7 +5,7 @@ public class ArrayMethods1 {
 	{
 		int[] list1 = {5, 2, 1, 7, 9, 4};
 		double[] list2 = {1.42, 2.5, 5.6, 4.7, 1.3, 3.45};
-		String[] list3 = {"a", "E", "b", "z", "u", "d"};
+		String[] list3 = {"a", "c", "b", "z", "u", "d"};
 		
 		//insertionSort(list1);
 		//selectionSort(list2);
@@ -28,17 +28,22 @@ public class ArrayMethods1 {
 	public static void bubbleSort(String[] list1)
 	{
 		String tempStr;
-		
-		for(int i=0;i<list1.length-1;i++)
+		//for(int z=0;isOrdered(list1)==false;z++)
+		int counter = 4;
+		while(counter>0)
 		{
-			if(list1[i].compareTo(list1[i+1])<0)
-			{
-				tempStr = list1[i];
-				list1[i] = list1[i+1];
-				list1[i+1] = tempStr;		 
+			for(int i=0;i<list1.length-1;i++)
+			{	
+				counter = 0;
+				if(list1[i].compareTo(list1[i+1])>0)
+				{
+					tempStr = list1[i];
+					list1[i] = list1[i+1];
+					list1[i+1] = tempStr;
+					counter++;
+				}
 			}
 		}
-		
 		
 	}
 	
@@ -47,6 +52,18 @@ public class ArrayMethods1 {
 		int temp = index1;
 		index1 = index2;
 		index2 = temp;
+	}
+	
+	public static boolean isOrdered(String[] list1)
+	{
+		for(int i=0;i<list1.length;i++)
+		{
+			if(list1[i].compareTo(list1[i+1])<0)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 }

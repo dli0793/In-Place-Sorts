14b59e75ae3ee9008f4f0d8f59c67030a0c7eba1
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-02-01 09:20:11 -0500

    :)

--- ArrayMethods/src/ArrayMethods1.java
+++ ArrayMethods/src/ArrayMethods1.java
@@ -7,16 +7,26 @@ public class ArrayMethods1 {
 		double[] list2 = {1.42, 2.5, 5.6, 4.7, 1.3, 3.45};
 		String[] list3 = {"a", "c", "b", "z", "u", "d"};
 		
-		//insertionSort(list1);
+		insertionSort(list1);
+		System.out.println(java.util.Arrays.toString(list1));
 		//selectionSort(list2);
-		bubbleSort(list3);
-		System.out.println(java.util.Arrays.toString(list3));
+		//bubbleSort(list3);
+		//System.out.println(java.util.Arrays.toString(list3));
 	}
 	
 	
 	public static void insertionSort(int[] list1)
 	{
-		
+		for(int i=1;i<list1.length;i++)
+		{
+			for(int j=i;j>0;j--)
+			{
+				if(list1[j]<list1[j-1])
+				{
+					swapIndex(j,j-1);
+				}
+			}
+		}
 	}
 	
 	
@@ -28,19 +38,19 @@ public class ArrayMethods1 {
 	public static void bubbleSort(String[] list1)
 	{
 		String tempStr;
-		while(isOrdered(list1)==false)
-		//int counter = 4;
-		//while(counter>0)
+		//while(isOrdered(list1)==false)
+		int counter = 4;
+		while(counter>0)
 		{
 			for(int i=0;i<list1.length-1;i++)
 			{	
-				//counter = 0;
+				counter = 0;
 				if(list1[i].compareTo(list1[i+1])>0)
 				{
 					tempStr = list1[i];
 					list1[i] = list1[i+1];
 					list1[i+1] = tempStr;
-					//counter++;
+					counter++;
 				}
 			}
 		}

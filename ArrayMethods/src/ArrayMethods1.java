f3072fe074d3d04d343034119443ab43d0319220
pjdan <pjdan@10.22.33.127>
2018-02-01 21:36:00 -0500

    :)

--- ArrayMethods/src/ArrayMethods1.java
+++ ArrayMethods/src/ArrayMethods1.java
@@ -3,15 +3,34 @@ public class ArrayMethods1 {
 
 	public static void main(String[] args) 
 	{
-		int[] list1 = {5, 2, 1, 7, 9, 4};
-		double[] list2 = {1.42, 2.5, 5.6, 4.7, 1.3, 3.45};
-		String[] list3 = {"a", "c", "b", "z", "u", "d"};
+		//Testing arrays
+		int[] list1 = {1, 4, 4, 5, 2, 4, 3, 17, 0};
+		double[] list2 = {1.1, 4.1, 4.2, 5.0, 2.0, 4.0, 3.0, 17.0, 0.0};
+		String[] list3 = {"zebra", "tortilla", "abba", "foo", "bar", "aba"};
 		
+		//Insertion Sort Test
+		long start = System.nanoTime();
 		insertionSort(list1);
+		long end = System.nanoTime();
+		long time = end-start;
+		System.out.println("Test1 took: " + time + "nanoseconds.");
 		System.out.println(java.util.Arrays.toString(list1));
-		//selectionSort(list2);
-		//bubbleSort(list3);
-		//System.out.println(java.util.Arrays.toString(list3));
+		
+		//Selection Sort Test
+		start = System.nanoTime();
+		selectionSort(list2);
+		end = System.nanoTime();
+		time = end-start;
+		System.out.println("Test2 took: " + time + "nanoseconds.");
+		System.out.println(java.util.Arrays.toString(list2));
+		
+		//Bubble Sort Test
+		start = System.nanoTime();
+		bubbleSort(list3);
+		end = System.nanoTime();
+		time = end-start;
+		System.out.println("Test3 took: " + time + "nanoseconds.");
+		System.out.println(java.util.Arrays.toString(list3));
 	}
 	
 	
@@ -23,7 +42,7 @@ public class ArrayMethods1 {
 			{
 				if(list1[j]<list1[j-1])
 				{
-					swapIndex(j,j-1);
+					swapIndex(j,j-1,list1);
 				}
 			}
 		}
@@ -32,48 +51,45 @@ public class ArrayMethods1 {
 	
 	public static void selectionSort(double[] list1)
 	{
-		
+		for(int i=0;i<list1.length;i++)
+		{
+			for(int j=i;j<list1.length-1;j++)
+			{
+				if(list1[i]>list1[j+1])
+				{
+					double temp = list1[i];
+					list1[i] = list1[j+1];
+					list1[j+1] = temp;
+				}
+			}
+		}
 	}
 	
 	public static void bubbleSort(String[] list1)
 	{
 		String tempStr;
-		//while(isOrdered(list1)==false)
-		int counter = 4;
+		int counter = 1;
 		while(counter>0)
 		{
+			counter = 0;
 			for(int i=0;i<list1.length-1;i++)
 			{	
-				counter = 0;
 				if(list1[i].compareTo(list1[i+1])>0)
 				{
 					tempStr = list1[i];
 					list1[i] = list1[i+1];
 					list1[i+1] = tempStr;
-					counter++;
+					counter = 1;
 				}
 			}
 		}
 		
 	}
 	
-	public static void swapIndex(int index1, int index2)
+	public static void swapIndex(int index1, int index2, int[] list1)
 	{
-		int temp = index1;
-		index1 = index2;
-		index2 = temp;
+		int temp = list1[index1];
+		list1[index1] = list1[index2];
+		list1[index2] = temp;
 	}
-	
-	public static boolean isOrdered(String[] list1)
-	{
-		for(int i=0;i<list1.length;i++)
-		{
-			if(list1[i].compareTo(list1[i+1])<0)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-
 }
